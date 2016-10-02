7ac2c549aff7bdbfdda2ff8211b569b26979fbfc
moncholo <moncholo06@gmail.com>
2016-10-02 14:19:19 -0300

    Subo correccion del test Chi Cuadrado

--- paq8l-Texto-FINAL/chi2/chi.py
+++ paq8l-Texto-FINAL/chi2/chi.py
@@ -11,8 +11,8 @@ str2 = f2.readline();
 f.close();
 f2.close();
 
-f_exp = [int(x) for x in str.split(';')];
-f_obs = [int(x) for x in str2.split(';')];
+f_exp = [float(x) for x in str.split(';')];
+f_obs = [float(x) for x in str2.split(';')];
 
 laplace =  len(f_obs) - len(f_exp);
 
@@ -25,7 +25,14 @@ if (len(f_exp) < len(f_obs)):
 	for x in range(0, laplace):
 		f_exp += [1];
 
+suma_observado = sum(f_obs)
+suma_esperado = sum(f_exp)
+
+for x in range(0, len(f_exp)):
+	f_exp[x] = f_exp[x]*suma_observado;
+	f_exp[x] = f_exp[x] / suma_esperado;
 
 (statistic, pvalue) = chisquare(f_obs,f_exp);
 
 print "statistic: %f	pvalue: %f	n-laplace: %d" %( statistic, pvalue,laplace);
+
