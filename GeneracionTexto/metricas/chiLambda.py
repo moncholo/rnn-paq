a04a3cab450eb7c9a47d2aec6fb14f6c1cb34343
moncholo <moncholo06@gmail.com>
2016-11-22 14:37:54 -0300

    Subo ChiSquare con lambda

---
+++ GeneracionTexto/metricas/chiLambda.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from scipy.stats import chisquare
+import sys
+
+f = open("entrada.frecuencias");
+f2 = open("generado.frecuencias");
+
+str = f.readline();
+str2 = f2.readline();
+
+f.close();
+f2.close();
+
+f_exp = [float(x) for x in str.split(';')];
+f_obs = [float(x) for x in str2.split(';')];
+
+laplace =  len(f_obs) - len(f_exp);
+
+
+#Aqui abajo hago la correcion de laplace
+if (len(f_exp) < len(f_obs)):
+	for x in range(0, laplace):
+		f_exp += [0];
+if (laplace > 0):
+	for x in range(0, len(f_exp)):
+		f_exp[x] += 0.00001;
+
+
+suma_observado = sum(f_obs)
+suma_esperado = sum(f_exp)
+
+for x in range(0, len(f_exp)):
+	f_exp[x] = f_exp[x]*suma_observado;
+	f_exp[x] = f_exp[x] / suma_esperado;
+
+(statistic, pvalue) = chisquare(f_obs,f_exp);
+
+print "%f,%d" %( statistic, laplace);
+
