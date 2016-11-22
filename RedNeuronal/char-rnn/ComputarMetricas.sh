f17598e42b25ae7c048b6dad9c1dbd53556abb3c
moncholo <moncholo06@gmail.com>
2016-11-21 22:06:12 -0300

    Subo resultados de metricas

--- RedNeuronal/char-rnn/ComputarMetricas.sh
+++ RedNeuronal/char-rnn/ComputarMetricas.sh
@@ -2,21 +2,21 @@
 
 
 for filePAQ in $1/PAQ/*; do
-
-	for fileRNN in $1/RedNeuronal/*; do
-		../../GeneracionTexto/metricas/Distribucion 4 $1/input.txt $fileRNN
-		resultado_chi=`../../GeneracionTexto/metricas/chi.py`
-		resultado_total=`../../GeneracionTexto/metricas/totalvariation.py`
-		../../GeneracionTexto/metricas/Distribucion 10 $1/input.txt $fileRNN
-		resultado_jaccard=`../../GeneracionTexto/metricas/jaccard.py`
-		echo $fileRNN "," $resultado_chi "," $resultado_total "," $resultado_jaccard >> ResultadosMetricas.txt
-
-	done
 	../../GeneracionTexto/metricas/Distribucion 4 $1/input.txt $filePAQ
 	resultado_chi=`../../GeneracionTexto/metricas/chi.py`
 	resultado_total=`../../GeneracionTexto/metricas/totalvariation.py`
 	../../GeneracionTexto/metricas/Distribucion 10 $1/input.txt $filePAQ
 	resultado_jaccard=`../../GeneracionTexto/metricas/jaccard.py`
 	echo $filePAQ "," $resultado_chi "," $resultado_total "," $resultado_jaccard  >> ResultadosMetricas.txt
+done
+
+for fileRNN in $1/RedNeuronal/*; do
+	../../GeneracionTexto/metricas/Distribucion 4 $1/input.txt $fileRNN
+	resultado_chi=`../../GeneracionTexto/metricas/chi.py`
+	resultado_total=`../../GeneracionTexto/metricas/totalvariation.py`
+	../../GeneracionTexto/metricas/Distribucion 10 $1/input.txt $fileRNN
+	resultado_jaccard=`../../GeneracionTexto/metricas/jaccard.py`
+	echo $fileRNN "," $resultado_chi "," $resultado_total "," $resultado_jaccard >> ResultadosMetricas.txt
 
 done
+
