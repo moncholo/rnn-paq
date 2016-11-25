eb1251dbeec96e6c0f52147536c904ccdef1a409
moncholo <moncholo06@gmail.com>
2016-11-25 18:33:21 -0300

    SUbo muestras de Math generadas por PAQ y actualizacion de excel

--- GeneracionTexto/paq8l/scriptPAQ.sh
+++ GeneracionTexto/paq8l/scriptPAQ.sh
@@ -1,99 +1,99 @@
 #!/bin/bash
 
-#cr_checkpoint --file CheckpointEnWik --term 4667
+#cr_checkpoint --file CheckpointMath --term 4667
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.70
-EnWik_PAQ_0.70.txt
+Math_PAQ_0.70.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.73
-EnWik_PAQ_0.73.txt
+Math_PAQ_0.73.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.75
-EnWik_PAQ_0.75.txt
+Math_PAQ_0.75.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.77
-EnWik_PAQ_0.77.txt
+Math_PAQ_0.77.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.79
-EnWik_PAQ_0.79.txt
+Math_PAQ_0.79.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.82
-EnWik_PAQ_0.82.txt
+Math_PAQ_0.82.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.85
-EnWik_PAQ_0.85.txt
+Math_PAQ_0.85.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.88
-EnWik_PAQ_0.88.txt
+Math_PAQ_0.88.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.91
-EnWik_PAQ_0.91.txt
+Math_PAQ_0.91.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.93
-EnWik_PAQ_0.93.txt
+Math_PAQ_0.93.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.94
-EnWik_PAQ_0.94.txt
+Math_PAQ_0.94.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.95
-EnWik_PAQ_0.95.txt
+Math_PAQ_0.95.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.96
-EnWik_PAQ_0.96.txt
+Math_PAQ_0.96.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.97
-EnWik_PAQ_0.97.txt
+Math_PAQ_0.97.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.98
-EnWik_PAQ_0.98.txt
+Math_PAQ_0.98.txt
 EOF
 
-cr_restart CheckpointEnWik << 'EOF'
+cr_restart CheckpointMath << 'EOF'
 100000
 0.99
-EnWik_PAQ_0.99.txt
+Math_PAQ_0.99.txt
 EOF
