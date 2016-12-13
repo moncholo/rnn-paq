f44ea19015211da6e05ece73dd58cb26adb982ae
moncholo <moncholo06@gmail.com>
2016-12-13 10:14:22 -0300

    RN Analisis de sentimientos

---
+++ RNN - Analisis de Sentimientos/script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+rm -f temp.txt
+
+cd stanford-corenlp-full-2016-10-31/
+
+for file in ../pos/*; do
+	java -cp "*" -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -file $file -output ROOT > temp.txt
+	negative=$(grep -c '^  Negative$' temp.txt)
+	verynegative=$(grep -c '^  Very negative$' temp.txt)
+	positive=$(grep -c '^  Positive$' temp.txt)
+	verypositive=$(grep -c '^  Very positive$' temp.txt)
+	neutral=$(grep -c '^  Neutral$' temp.txt)
+
+	suma=$((0*neutral + 1*positive + 2*verypositive - 1*negative - 2*verynegative))
+	echo "Archivo: $file, Es: Positivo, Negative: $negative, Positive: $positive, Very negative: $verynegative, Very positive: $verypositive, Neutral: $neutral, Suma: $suma" >> Resultados.txt
+
+	rm -f temp.txt
+	mv $file ../Procesados/pos
+done
+
+for file in ../neg/*; do
+	java -cp "*" -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -file $file -output ROOT > temp.txt
+	negative=$(grep -c '^  Negative$' temp.txt)
+	verynegative=$(grep -c '^  Very negative$' temp.txt)
+	positive=$(grep -c '^  Positive$' temp.txt)
+	verypositive=$(grep -c '^  Very positive$' temp.txt)
+	neutral=$(grep -c '^  Neutral$' temp.txt)
+
+	suma=$((0*neutral + 1*positive + 2*verypositive - 1*negative - 2*verynegative))
+	echo "Archivo: $file, Es: Negativo, Negative: $negative, Positive: $positive, Very negative: $verynegative, Very positive: $verypositive, Neutral: $neutral, Suma: $suma" >> Resultados.txt
+
+	rm -f temp.txt
+	mv $file ../Procesados/neg
+
+done
+
+
+
+
+
