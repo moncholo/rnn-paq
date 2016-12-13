83aad4640a7dfe5f4a622d66c349eedd4072c791
moncholo <moncholo06@gmail.com>
2016-12-13 16:29:39 -0300

    Saco carpetas que no sirven

--- GeneracionTexto/paq8l/scriptPAQ.sh
+++ GeneracionTexto/paq8l/scriptPAQ.sh
@@ -2,100 +2,102 @@
 
 echo "Empieza"
 
-cr_restart CheckpointShakespare << 'EOF'
-100000
-0.94
-Shakespare_PAQ_0.94.txt
-EOF
 
-cr_restart CheckpointShakespare << 'EOF'
-100000
-0.95
-Shakespare_PAQ_0.95.txt
-EOF
-
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.96
-Shakespare_PAQ_0.96.txt
+WarEspecial_PAQ_0.96.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.97
-Shakespare_PAQ_0.97.txt
+WarEspecial_PAQ_0.97.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.98
-Shakespare_PAQ_0.98.txt
+WarEspecial_PAQ_0.98.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.99
-Shakespare_PAQ_0.99.txt
+WarEspecial_PAQ_0.99.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.70
-Shakespare_PAQ_0.70.txt
+WarEspecial_PAQ_0.70.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.73
-Shakespare_PAQ_0.73.txt
+WarEspecial_PAQ_0.73.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.75
-Shakespare_PAQ_0.75.txt
+WarEspecial_PAQ_0.75.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.77
-Shakespare_PAQ_0.77.txt
+WarEspecial_PAQ_0.77.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.79
-Shakespare_PAQ_0.79.txt
+WarEspecial_PAQ_0.79.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.82
-Shakespare_PAQ_0.82.txt
+WarEspecial_PAQ_0.82.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.85
-Shakespare_PAQ_0.85.txt
+WarEspecial_PAQ_0.85.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.88
-Shakespare_PAQ_0.88.txt
+WarEspecial_PAQ_0.88.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.91
-Shakespare_PAQ_0.91.txt
+WarEspecial_PAQ_0.91.txt
 EOF
 
-cr_restart CheckpointShakespare << 'EOF'
+cr_restart CheckpointWarEspecial << 'EOF'
 100000
 0.93
-Shakespare_PAQ_0.93.txt
+WarEspecial_PAQ_0.93.txt
 EOF
+
+cr_restart CheckpointWarEspecial << 'EOF'
+100000
+0.94
+WarEspecial_PAQ_0.94.txt
+EOF
+
+cr_restart CheckpointWarEspecial << 'EOF'
+100000
+0.95
+WarEspecial_PAQ_0.95.txt
+EOF
+
 
 
